052cf995261846360bcc4d7130be4d800c1aa693
lucasbasso92 <lucasbasso92@hotmail.com>
2025-09-02 21:31:58 -0300

    aula-testee2e-externo-kaique

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,34 @@
+describe("Tela de login", function() {
+
+beforeEach(function() {
+  
+    cy.visit("https://kaiquecovo.github.io/pagina-para-teste-e2e")
+})
+
+
+
+it("Deve fazer login com sucesso", function() {
+    cy.get("#username").type("admin")
+    
+    cy.get("#password").type("1234")
+    
+    cy.get('button').click()
+
+    cy.url().should("include", "sucesso.html")
+
+    cy.contains("Você logou com sucesso").should("be.visible")
+})
+
+it("Deve falhar o login", function() {
+    cy.get("#username").type("usuario_errado")
+
+    cy.get("#password").type("senha_errada")
+
+    cy.get('button').click()
+
+    
+
+    cy.get("#errorMessage").should('contain', "Usuário ou senha incorretos")
+
+})
+})
